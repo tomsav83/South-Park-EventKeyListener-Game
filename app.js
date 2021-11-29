40af74a9c5f8cd1264184a1740f100687b6b80b9
Tomsav83 <tomsav83@gmail.com>
2021-11-29 20:09:57 +0000

    final

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 window.addEventListener('keydown', (e) => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  let content = document.getElementsByClassName('key');
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
